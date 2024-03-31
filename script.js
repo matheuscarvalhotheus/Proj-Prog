ee5c0970f0c325c545032f7736e917aef9980d72
_carvalho_ <78558082+matheuscarvalhotheus@users.noreply.github.com>
2024-03-30 21:19:30 -0300

    Revert "imagens"

---
+++ script.js
@@ -0,0 +1,22 @@
+function enviar(e){
+    e.preventDefault();
+}
+function teste(){
+ /* var resposta = document.getElementById("resposta").value;]
+  
+  if (resposta === mob.name) {
+  alert("acertou");
+}
+  else {
+  alert("tente novamente");
+  
+  */
+  var naleatorio=Math.floor(Math.random() * 10);
+  document.getElementById("mob").src = mob.mobs[naleatorio].img;
+  document.getElementById("nome").innerHTML = mob.mobs[naleatorio].nome;
+}
+var naleatorio=Math.floor(Math.random() * 10);
+const response = await fetch('jsons/mob.json');
+const mob = await response.json();
+document.getElementById("mob").src = mob.mobs[naleatorio].img;
+document.getElementById("nome").innerHTML = mob.mobs[naleatorio].nome;
