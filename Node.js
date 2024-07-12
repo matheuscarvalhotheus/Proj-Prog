1e407ab0194b54c549bf14ad818adf97da89d0a3
Carvalho <matheus.carvalho.santos23@gmail.com>
2024-07-12 19:25:38 -0300

    tentei fazer a parte do back

---
+++ Node.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+
+app.use(express.json());
+
+// Array para armazenar dados temporários
+let dadosTemporarios = [];
+
+// Rota para obter todos os dados
+app.get('/dados', (req, res) => {
+  res.json(dadosTemporarios);
+});
+
+// Rota para adicionar novos dados
+app.post('/dados', (req, res) => {
+  const novoDado = req.body;
+  dadosTemporarios.push(novoDado);
+  res.status(201).json(novoDado);
+});
+
+// Rota para limpar todos os dados
+app.delete('/dados', (req, res) => {
+  dadosTemporarios = [];
+  res.status(204).end();
+});
+
+// Iniciar o servidor
+app.listen(PORT, () => {
+  console.log(`Servidor está rodando em http://localhost:${PORT}`);
+});
