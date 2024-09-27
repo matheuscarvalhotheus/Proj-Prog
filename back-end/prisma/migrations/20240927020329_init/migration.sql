a18b9ea1abca49cddaa5252e3efb233d9f347ebc
Gtagusta <fariasmarquesgustavo@gmail.com>
2024-09-26 23:18:02 -0300

    schema pronto

---
+++ back-end/prisma/migrations/20240927020329_init/migration.sql
@@ -0,0 +1,107 @@
+-- CreateTable
+CREATE TABLE "gamemode" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "hearts" (
+    "modeId" INTEGER NOT NULL,
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "img" TEXT NOT NULL,
+    CONSTRAINT "hearts_modeId_fkey" FOREIGN KEY ("modeId") REFERENCES "gamemode" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "difficulty" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "game" (
+    "modeId" INTEGER NOT NULL,
+    "difficultyId" INTEGER NOT NULL,
+
+    PRIMARY KEY ("modeId", "difficultyId"),
+    CONSTRAINT "game_modeId_fkey" FOREIGN KEY ("modeId") REFERENCES "gamemode" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "game_difficultyId_fkey" FOREIGN KEY ("difficultyId") REFERENCES "difficulty" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "gameSettings" (
+    "modeId" INTEGER NOT NULL,
+    "difficultyId" INTEGER NOT NULL,
+    "miniId" INTEGER NOT NULL,
+    "tries" INTEGER NOT NULL,
+
+    PRIMARY KEY ("modeId", "difficultyId", "miniId"),
+    CONSTRAINT "gameSettings_modeId_difficultyId_fkey" FOREIGN KEY ("modeId", "difficultyId") REFERENCES "game" ("modeId", "difficultyId") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "gameSettings_miniId_fkey" FOREIGN KEY ("miniId") REFERENCES "minigame" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "minigame" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "icon" TEXT NOT NULL,
+    "logo" TEXT NOT NULL,
+    "background" TEXT NOT NULL,
+    "border" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "solutions" (
+    "miniId" INTEGER NOT NULL,
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "answer" TEXT NOT NULL,
+    "img" TEXT NOT NULL,
+    CONSTRAINT "solutions_miniId_fkey" FOREIGN KEY ("miniId") REFERENCES "minigame" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "user" (
+    "email" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "typeId" INTEGER NOT NULL,
+    CONSTRAINT "user_typeId_fkey" FOREIGN KEY ("typeId") REFERENCES "userType" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "userType" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "player" (
+    "userEmail" TEXT NOT NULL,
+    "miniId" INTEGER NOT NULL,
+
+    PRIMARY KEY ("userEmail", "miniId"),
+    CONSTRAINT "player_userEmail_fkey" FOREIGN KEY ("userEmail") REFERENCES "user" ("email") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "player_miniId_fkey" FOREIGN KEY ("miniId") REFERENCES "minigame" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "playerData" (
+    "userEmail" TEXT NOT NULL,
+    "miniId" INTEGER NOT NULL,
+    "modeId" INTEGER NOT NULL,
+    "points" INTEGER NOT NULL,
+
+    PRIMARY KEY ("userEmail", "miniId", "modeId"),
+    CONSTRAINT "playerData_userEmail_miniId_fkey" FOREIGN KEY ("userEmail", "miniId") REFERENCES "player" ("userEmail", "miniId") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "playerData_modeId_fkey" FOREIGN KEY ("modeId") REFERENCES "gamemode" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "hearts_modeId_id_key" ON "hearts"("modeId", "id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "solutions_miniId_id_key" ON "solutions"("miniId", "id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_email_typeId_key" ON "user"("email", "typeId");
